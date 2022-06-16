39f0a3a6915c3c655446a5dbbdeb958c4f118fcc
plowww <kevin_g@my.uri.edu>
2022-06-16 17:15:24 -0400

    Finished Classes

--- src/lib/product.ts
+++ src/lib/product.ts
@@ -1,14 +1,25 @@
 import { Product } from './interfaces';
 
-// This will act as the foundation for other Product type classes (FoodProduct, SportingProduct)
 abstract class ProductBase implements Product {
-  constructor(public id: number, public name: string, public icon: string) {}
-  validate(): boolean {
-    throw new Error('Not implemented');
-  }
+    // properties/fields
+
+  // constructor (auto implemented properties)
+  constructor(
+    public id: number,
+    public name: string,
+    public icon: string) { }
+
+    validate(): boolean {
+      throw new Error('Not implemented');
+    }
 }
 
 export class FoodProduct extends ProductBase {
+  // properties/fields
+
+  // constructor (auto implemented properties)
+  
+  // functions/methods
   validate(): boolean {
     return !!this.id && !!this.name && !!this.icon;
   }
